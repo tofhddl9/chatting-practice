8779f630a70efee4d0e924389330d56a59d7351c
tofhddl9 <tofhddl9@gmail.com>
2018-01-07 14:10:44 +0000

    Add writen()

--- server_select.c
+++ server_select.c
@@ -13,13 +13,15 @@
 static const int bufSize = 1024;
 
 int readn(int fd, char *buf, short readLen);
+int writen(int fd, char *buf, short n);
+
 int main(void)
 {
   int listenFD, connectFD;
   struct sockaddr_in serverAddr, clientAddr;
   int clientAddrLen;
   int FDNum;
-  int i;
+  int i, n;
 
   char buf[bufSize];
   fd_set readFDs, copyFDs; 
@@ -44,7 +46,7 @@ int main(void)
     exit(1);
   }
   
-  if (listen(listenFD, 5) < 0) {
+  if (listen(listenFD, 128) < 0) {
     perror("listen() error");
     exit(1);
   }
@@ -52,10 +54,9 @@ int main(void)
   FD_ZERO(&readFDs);
   FD_SET(listenFD, &readFDs);
   int maxFD = listenFD;
-
   while (1) {
     copyFDs = readFDs;
-    
+    //printf("maxFD : %d\n", maxFD); 
     FDNum = select(maxFD+1, &copyFDs, NULL, NULL, NULL);
    
     if (FDNum == 0)
@@ -73,24 +74,25 @@ int main(void)
         maxFD = connectFD;
       }
     }
-    
+
     for (i=0;i<=maxFD;++i) {
+      if (i == listenFD) continue;
       if (FD_ISSET(i, &copyFDs)) {
         memset(&buf, 0, sizeof(buf));
-        if (readn(i, buf, 2) == 0) {
-          break;
-        }
-        short recvLen = *(short *)&buf;
-        printf("[%d] recvLen : %d\n",i,recvLen);
-        if (readn(i, buf, recvLen) == 0) {
-          printf("close\n");
-          close(i);
+        if ((n = readn(i, buf, 2)) == 0) {
+          //printf("client %d close\n",i);
           FD_CLR(i, &readFDs);
         }
-        else {
-          buf[recvLen] = 0;
-          write(i, buf, strlen(buf));
+        short recvLen = *(short *)&buf;
+        
+        if ((n = readn(i, buf, recvLen)) == 0) {
+          printf("client %d close\n",i);
+          FD_CLR(i, &readFDs);
         }
+        
+        write(i, &recvLen, 2);
+        buf[recvLen] = 0;
+        writen(i, buf, strlen(buf));
       }
     }
   }
@@ -102,11 +104,37 @@ int readn(int fd, char *buf, short readLen)
   short sp = 0, readed;
   while (readLen) {
     readed = read(fd, buf + sp, readLen);
-    if (readed <= 0) {
+    if (readed < 0) {
+      perror("read error");
+      exit(1);
+    }
+    else if (readed == 0) {
       return 0;
     }
     readLen -= readed;
     sp += readed;
   }
+  if (readed == 0) {
+    return 0;
+  }
+  return 1;
+}
+
+int writen(int fd, char *buf, short n)
+{
+  short sp = 0, wroted;
+  while (n) {
+    wroted = write(fd, buf+sp, n);
+    if (wroted < 0) {
+      perror("write error");
+      exit(1);
+    }
+    else if (wroted == 0)
+      return 0;
+    sp += wroted;
+    n -= wroted;
+  }
+  if (wroted == 0)
+    return 0;
   return 1;
 }
